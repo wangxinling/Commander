d3f6c9582f0c8a12b50da05212f9a7a3aa0b967d
mikewang.xinling@gmail.com <Wangle17>
2024-10-13 00:31:01 +1100

    Added the webllm instead of onnx

---
+++ backend/public/jsfiles/index.59889c3c5361d6fcc2fa.js
@@ -0,0 +1,215 @@
+import * as __WEBPACK_EXTERNAL_MODULE_https_esm_run_mlc_ai_web_llm_c4b7b6a0__ from "https://esm.run/@mlc-ai/web-llm";
+/******/ var __webpack_modules__ = ({
+
+/***/ "https://esm.run/@mlc-ai/web-llm":
+/*!**************************************************!*\
+  !*** external "https://esm.run/@mlc-ai/web-llm" ***!
+  \**************************************************/
+/***/ ((module) => {
+
+module.exports = __WEBPACK_EXTERNAL_MODULE_https_esm_run_mlc_ai_web_llm_c4b7b6a0__;
+
+/***/ })
+
+/******/ });
+/************************************************************************/
+/******/ // The module cache
+/******/ var __webpack_module_cache__ = {};
+/******/ 
+/******/ // The require function
+/******/ function __webpack_require__(moduleId) {
+/******/ 	// Check if module is in cache
+/******/ 	var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 	if (cachedModule !== undefined) {
+/******/ 		return cachedModule.exports;
+/******/ 	}
+/******/ 	// Create a new module (and put it into the cache)
+/******/ 	var module = __webpack_module_cache__[moduleId] = {
+/******/ 		// no module.id needed
+/******/ 		// no module.loaded needed
+/******/ 		exports: {}
+/******/ 	};
+/******/ 
+/******/ 	// Execute the module function
+/******/ 	__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 
+/******/ 	// Return the exports of the module
+/******/ 	return module.exports;
+/******/ }
+/******/ 
+/************************************************************************/
+/******/ /* webpack/runtime/make namespace object */
+/******/ (() => {
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = (exports) => {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/ })();
+/******/ 
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var https_esm_run_mlc_ai_web_llm__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! https://esm.run/@mlc-ai/web-llm */ "https://esm.run/@mlc-ai/web-llm");
+
+
+/*************** WebLLM logic ***************/
+const messages = [
+  {
+    content: "You are a helpful AI agent helping users.",
+    role: "system",
+  },
+];
+
+const availableModels = https_esm_run_mlc_ai_web_llm__WEBPACK_IMPORTED_MODULE_0__.prebuiltAppConfig.model_list.map(
+  (m) => m.model_id,
+);
+let selectedModel = "Llama-3.1-8B-Instruct-q4f32_1-1k";
+
+// Callback function for initializing progress
+function updateEngineInitProgressCallback(report) {
+  console.log("initialize", report.progress);
+  document.getElementById("download-status").textContent = report.text;
+}
+
+// Create engine instance
+const engine = new https_esm_run_mlc_ai_web_llm__WEBPACK_IMPORTED_MODULE_0__.MLCEngine();
+engine.setInitProgressCallback(updateEngineInitProgressCallback);
+
+async function initializeWebLLMEngine() {
+  document.getElementById("download-status").classList.remove("hidden");
+  selectedModel = document.getElementById("model-selection").value;
+  const config = {
+    temperature: 1.0,
+    top_p: 1,
+  };
+  await engine.reload(selectedModel, config);
+}
+
+async function streamingGenerating(messages, onUpdate, onFinish, onError) {
+  try {
+    let curMessage = "";
+    let usage;
+    const completion = await engine.chat.completions.create({
+      stream: true,
+      messages,
+      stream_options: { include_usage: true },
+    });
+    for await (const chunk of completion) {
+      const curDelta = chunk.choices[0]?.delta.content;
+      if (curDelta) {
+        curMessage += curDelta;
+      }
+      if (chunk.usage) {
+        usage = chunk.usage;
+      }
+      onUpdate(curMessage);
+    }
+    const finalMessage = await engine.getMessage();
+    onFinish(finalMessage, usage);
+  } catch (err) {
+    onError(err);
+  }
+}
+
+/*************** UI logic ***************/
+function onMessageSend() {
+  const input = document.getElementById("user-input").value.trim();
+  const message = {
+    content: input,
+    role: "user",
+  };
+  if (input.length === 0) {
+    return;
+  }
+  document.getElementById("send").disabled = true;
+
+  messages.push(message);
+  appendMessage(message);
+
+  document.getElementById("user-input").value = "";
+  document
+    .getElementById("user-input")
+    .setAttribute("placeholder", "Generating...");
+
+  const aiMessage = {
+    content: "typing...",
+    role: "assistant",
+  };
+  appendMessage(aiMessage);
+
+  const onFinishGenerating = (finalMessage, usage) => {
+    updateLastMessage(finalMessage);
+    document.getElementById("send").disabled = false;
+    const usageText =
+      `prompt_tokens: ${usage.prompt_tokens}, ` +
+      `completion_tokens: ${usage.completion_tokens}, ` +
+      `prefill: ${usage.extra.prefill_tokens_per_s.toFixed(4)} tokens/sec, ` +
+      `decoding: ${usage.extra.decode_tokens_per_s.toFixed(4)} tokens/sec`;
+    document.getElementById("chat-stats").classList.remove("hidden");
+    document.getElementById("chat-stats").textContent = usageText;
+  };
+
+  streamingGenerating(
+    messages,
+    updateLastMessage,
+    onFinishGenerating,
+    console.error,
+  );
+}
+
+function appendMessage(message) {
+  const chatBox = document.getElementById("chat-box");
+  const container = document.createElement("div");
+  container.classList.add("message-container");
+  const newMessage = document.createElement("div");
+  newMessage.classList.add("message");
+  newMessage.textContent = message.content;
+
+  if (message.role === "user") {
+    container.classList.add("user");
+  } else {
+    container.classList.add("assistant");
+  }
+
+  container.appendChild(newMessage);
+  chatBox.appendChild(container);
+  chatBox.scrollTop = chatBox.scrollHeight; // Scroll to the latest message
+}
+
+function updateLastMessage(content) {
+  const messageDoms = document
+    .getElementById("chat-box")
+    .querySelectorAll(".message");
+  const lastMessageDom = messageDoms[messageDoms.length - 1];
+  lastMessageDom.textContent = content;
+}
+
+/*************** UI binding ***************/
+availableModels.forEach((modelId) => {
+  const option = document.createElement("option");
+  option.value = modelId;
+  option.textContent = modelId;
+  document.getElementById("model-selection").appendChild(option);
+});
+document.getElementById("model-selection").value = selectedModel;
+document.getElementById("download").addEventListener("click", function () {
+  initializeWebLLMEngine().then(() => {
+    document.getElementById("send").disabled = false;
+  });
+});
+document.getElementById("send").addEventListener("click", function () {
+  onMessageSend();
+});
+
+})();
+
+
+//# sourceMappingURL=index.59889c3c5361d6fcc2fa.js.map
--- webllm/webpack.config.js
+++ webllm/webpack.config.js
@@ -9,43 +9,48 @@ import FileManagerPlugin from 'filemanager-webpack-plugin';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // Change to parent directory
-const __parentDir = path.join(__dirname, '../backend/public');
+const __backendDir = path.join(__dirname, '../backend/public');
 const __viewDir = path.join(__dirname, '../backend/app/Views');
+const __jsfileDir = path.join(__dirname, '/dist/jsfiles');
 
 export default {
     mode: 'development',
     devtool: 'source-map',
     entry: {
-        'dist/main': './main.js',
-        'dist/main.min': './main.js',
+        'index': './src/index.js',
     },
     output: {
         filename: '[name].[contenthash].js',
-        path: __dirname,
+        path: __jsfileDir,
         library: {
             type: 'module',
         },
+        clean: true,
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html',
-            filename: 'index.html',
-        }),
-        // Copy .wasm files to dist folder
-        new CopyWebpackPlugin({
-            patterns: [
-                {
-                    from: 'node_modules/onnxruntime-web/dist/*.jsep.*',
-                    to: 'dist/[name][ext]'
-                },
-            ],
+            filename: '../index.html',
+            scriptLoading: 'module'
         }),
         new FileManagerPlugin({
             events: {
+              onStart: { 
+                delete: [
+                 {
+                   source: path.join(__backendDir,'jsfiles/'), 
+                    options:{
+                      force: true,
+                      recursive : true,
+                      },
+                  },
+                ],
+              },
               onEnd: {
                 copy: [
-                    { source:  path.resolve(__dirname,'dist/*'), destination: path.resolve(__parentDir,'dist/') },
-                    { source: 'index.html', destination: path.resolve(__viewDir,'index.html') },
+                    { source:  path.resolve(__dirname,'dist/jsfiles/*'), destination: path.resolve(__backendDir,'jsfiles/') },
+                    { source:  path.resolve(__dirname,'dist/index.html'), destination: path.resolve(__viewDir,'index.html') },
+                    { source:  path.resolve(__dirname,'src/index.css'), destination: path.resolve(__backendDir,'index.css') },
                   ],
 
                 },
